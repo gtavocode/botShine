8c064e9e2fb19f5dfef0c68a9183828a464cbce2
Gustavo <gtavo.more@gmail.com>
2020-05-02 21:56:57 -0500

    Update keys.js
    Se modificó el token para el server.

--- keys.js
+++ keys.js
@@ -1,4 +1,3 @@
 module.exports = {
-    discordToken: 'MjY1MzA2MjYzNjI4NDE0OTc3.C0tNTA.lYItaVPfhc7fgftqR6TBbiuqh8c',
-    lastfmApiKey: 'f5140bbbbd4c60aa665e616964591622'
+    discordToken: 'MjY1MzA2MjYzNjI4NDE0OTc3.Xq4xiQ.uszSVCKnfJpE9GC5cEJ7pJ5T6qg'
 }
